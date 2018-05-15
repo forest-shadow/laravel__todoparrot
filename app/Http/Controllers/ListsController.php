74f3680373660e7fb92249d3f2f0ebef44fd6f63
forest-shadow <mamasay@yandex.ru>
2018-05-15 10:18:38 +0300

    Integrate TodoList model with ListsController
    * display currently added to db TodoLists in 'lists.index' view
    * fix 'lists' route

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TodoList;
 
 class ListsController extends Controller
 {
@@ -13,7 +14,8 @@ class ListsController extends Controller
      */
     public function index()
     {
-        return view('lists.index');
+        $lists = TodoList::all();
+        return view('lists.index')->withLists($lists);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('lists', 'ListsController@index');
+Route::resource('lists', 'ListsController');
